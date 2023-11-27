3dcf201c66b70713ca238d8009077dbf91047caa
Jaz Ku <jsku@dons.usfca.edu>
2023-11-26 23:26:48 -0800

    todo: figure how to import multimap

--- Country.java
+++ Country.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Country implements Comparable<Country{
+public class Country implements Comparable<Country>{
     private ArrayList<String> countryNames;
-    private String countryCode;
-    private HashMap<Country> neighbours;
+    private String countryCode;         //representative code
+    private HashMap<String, Country> neighbours;
     private int distanceFromSource;     //all distances in files are whole numbers
 
-    //use default constructor for now
+    //Constructor for countries with >2 aliases
+    public Country(ArrayList<String> countryNames, String countryCode) {
+        this.countryNames = countryNames;
+        this.countryCode = countryCode;
+    }
 
     @Override
     public int compareTo(Country c) {
--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,25 +1,45 @@
 import java.io.IOException;
 import java.util.HashMap;
+//ask prof how to import multimap wth
+//import java.com.google.common.collect.ArrayListMultimap;
+import java.util.StringTokenizer;
+import java.io.*;
 
 public class IRoadTrip {
-    private HashMap<Country> countriesGraph;
-    public IRoadTrip (String [] args) {
-        String[] validArgs = {"borders.txt", "capdist.csv", "state_name.tsv"};
-
-        for (String arg : args) {
-            System.out.println(arg);
-        }
+    private HashMap<String, String[]> stateNameDict;
+    private HashMap<String, Country> countriesGraph;
+    public IRoadTrip (String [] args) throws IOException {
+        FileReader borders = new FileReader(args[0]);
+        FileReader capdist = new FileReader(args[1]);
+        FileReader stateName = new FileReader(args[2]);
     }
 
-    private void fileToHashMap() {
+    private void createStateNameDict() {
         //Handle countries with multiple names
         //First entry of alias list is the representative country code
         String[] DRCAliases = {"DRC", "Congo, Democratic Republic of", "Congo, Democratic Republic of the", "Democratic Republic of the Congo"};
         String[] PRKAliases = {"PRK", "North Korea", "Korea, North"};
         String[] ROKAliases = {"ROK", "South Korea", "Korea, South"};
         String[] MYAAliases = {"MYA", "Myanmar, Burma"};
-        String[] DRVAliases = {"DRV", "VNM", "RVN", "Vietnam"}
+        String[] DRVAliases = {"DRV", "VNM", "RVN", "Vietnam"};
         String[] TANAliases = {"TAZ", "ZAN", "Tanzania", "Tanganyika", "Zanzibar"};
+
+        String[] countryCodeExceptionList = {"DRC", "PRK", "ROK", "MYA", "DRV", "TAZ"};
+
+    }
+
+    public void generateAliasList() {
+        StringTokenizer st = new StringTokenizer("Turkey (Turkiye)");
+
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            System.out.println(token);
+
+            if (token.contains("(")) {
+                token = token.substring(1, token.length() - 1);
+            }
+            System.out.println(token);
+        }
     }
 
     /*
@@ -39,14 +59,23 @@ public class IRoadTrip {
         // Replace with your code
         System.out.println("IRoadTrip - skeleton");
     }
-    */
 
    public HashMap<String> getCountriesGraph() {
         return this.countriesGraph;
    }
+   */
 
     public static void main(String[] args) throws IOException {
-        IRoadTrip a3 = new IRoadTrip(args);
+        if (args.length == 3) {
+            try {
+                IRoadTrip a3 = new IRoadTrip(args);
+            } catch (FileNotFoundException e) {
+                System.out.println(e + "\nHalting execution...");
+                System.exit(-1);
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
 
