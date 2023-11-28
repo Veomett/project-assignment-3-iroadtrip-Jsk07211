abfb37500df2cb70641e1f93c22a0e21129abc80
Jaz Ku <jsku@dons.usfca.edu>
2023-11-27 20:19:45 -0800

    missing country codes

--- Country.java
+++ Country.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
 
 public class Country implements Comparable<Country>{
     private String repName;                         //representative name
-    private HashMap<String, String> neighbours;    //countryName of neighbours and distance
-    private double distanceFromSource;   
+    private HashMap<String, Double> neighbours;    //countryName of neighbours and distance
+    private Double distanceFromSource;   
 
     //Constructor for countries with >2 aliases
     public Country(String repName) {
         this.repName = repName;
-        this.distanceFromSource = Integer.MAX_VALUE;
-        this.neighbours = new HashMap<String, String>();
+        this.distanceFromSource = Double.POSITIVE_INFINITY;
+        this.neighbours = new HashMap<String, Double>();
+    }
+
+    public void addNeighbour(String key, Double value) {
+        if (!neighbours.containsKey(key)){
+            neighbours.put(key, value);
+        }
+    }
+
+    public String getRepName() {
+        return this.repName;
     }
 
     @Override
@@ -18,12 +30,17 @@ public class Country implements Comparable<Country>{
         return Double.compare(this.distanceFromSource, c.distanceFromSource);
     }
 
-    //Identified by its representative name
     @Override
-    public boolean equals(Object o) {
-        if (this.repName == o) {
-            return true;
+    public String toString() {
+        Set keys = neighbours.keySet();
+        Iterator i = keys.iterator();
+        String toReturn = "\nNeighbours: ";
+
+        while (i.hasNext()) {
+            Object key = i.next();
+            toReturn += "neighbours[" + key + "]: " + neighbours.get(key) + "; ";
         }
-        return false;
+        toReturn += "\n";
+        return toReturn;
     }
 }
--- IRoadTrip.java
+++ IRoadTrip.java
@@ -4,6 +4,8 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Set;
+import java.util.Iterator;
 import java.io.*;
 
 public class IRoadTrip {
@@ -15,13 +17,16 @@ public class IRoadTrip {
         FileReader stateNames = new FileReader(args[2]);
 
         nameDict = new HashMap<String, String>();
-        populateNameDict(borders, capdist, stateNames);
+        countriesGraph = new HashMap<String, Country>();
+        populateHashMaps(borders, capdist, stateNames);
     }
 
-    private void populateNameDict(FileReader borders, FileReader capdist, FileReader stateNames) {
+    private void populateHashMaps(FileReader borders, FileReader capdist, FileReader stateNames) {
         createEntriesForNameExceptions();
         createStateNameEntries(stateNames);
         createBorderNameEntries(borders);
+        getStateNameDistances(capdist);
+        //viewHashMap();
     }
 
     private void createStateNameEntries(FileReader stateNames) {
@@ -36,7 +41,9 @@ public class IRoadTrip {
                 //state_name.tsv == tab separated values
                 String[] fieldVals = line.split("\t", 0);
 
-                generateAliasEntries(fieldVals[2]);
+                //no need to capture key value since no distances are found in this file
+                //also generate an alias for the code
+                generateAliasEntries(fieldVals[2] + "/" + fieldVals[1]);
 
                 line = reader.readLine();
             }
@@ -60,10 +67,9 @@ public class IRoadTrip {
 
             while (line != null) {
                 String[] fieldVals = line.split("=|;", 0);
+                String countryKey = generateAliasEntries(fieldVals[0]);
 
                 for (int i = 1; i < fieldVals.length; i++) {
-                    System.out.println(fieldVals[i]);
-
                     if (fieldVals[i].strip().length() == 0) {
                         continue;
                     }
@@ -71,20 +77,62 @@ public class IRoadTrip {
                     matchAlias = hasAlias.matcher(fieldVals[i].strip());
                     matchNoAlias = noAlias.matcher(fieldVals[i].strip());
 
+                    String toAdd = "";
+                    double distanceFromSource = 0;
+
                     if (matchAlias.find()) {
-                        System.out.println(matchAlias.group(1));
-                        System.out.println(matchAlias.group(2));
-                        System.out.println("Hello");
-                    } else if (matchNoAlias.find()) {
-                        System.out.println(matchNoAlias.group(1));
-                        System.out.println(matchNoAlias.group(2));
-                    } else {
-                        System.out.println("No match");
-                    }
+                        toAdd = matchAlias.group(1);
+                        distanceFromSource = Double.parseDouble(matchAlias.group(2).replaceAll(",", ""));
+                    } 
+                    
+                    if (matchNoAlias.find()) {
+                        toAdd = matchNoAlias.group(1);
+                        distanceFromSource = Double.parseDouble(matchNoAlias.group(2).replaceAll(",", ""));
+                    } 
+
+                    String neighbourKey = generateAliasEntries(toAdd);
+                    countriesGraph.get(countryKey).addNeighbour(neighbourKey, distanceFromSource);
+
                 }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getStateNameDistances(FileReader capdist) {
+        BufferedReader reader;
+
+        try{
+            reader = new BufferedReader(capdist);
+            String line = reader.readLine();    //skip first line
+
+            line = reader.readLine();
+            while (line != null) {
+                //capdist.csv == comma separated values
+                String[] fieldVals = line.split(",", 0);
+
+                for (String val : fieldVals) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+
+                System.out.println(fieldVals[1]);
+
+                //uses country codes from state_name.tsv
+                String countryKey = nameDict.get(fieldVals[1]);
+                Double distanceFromSource = Double.parseDouble(fieldVals[4]);
+                String neighbourKey = nameDict.get(fieldVals[3]);
+
+                Country c = countriesGraph.get(countryKey);
+                System.out.println(c);
+                c.addNeighbour(neighbourKey, distanceFromSource);
+                System.out.println(c);
+                
 
                 line = reader.readLine();
-                //break; //**remove later**
+                break;
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -92,17 +140,22 @@ public class IRoadTrip {
     }
 
     //creates keys for states with difficult to detect aliases
+    //create keys for country codes not in state_name.tsv
     public void createEntriesForNameExceptions() {
         //First entry of alias list is the representative country code
         String[] DRCAliases = {"Congo, Democratic Republic of", "Congo, Democratic Republic of the", "Democratic Republic of the Congo", "DRC"};
         String[] PRKAliases = {"North Korea", "Korea, North", "PRK"};
-        String[] ROKAliases = {"South Korea", "Korea, South", "ROK"};
+        String[] ROKAliases = {"South Korea", "Korea, South", "Korea", "ROK", "KOR"};
         String[] MYAAliases = {"Myanmar", "Burma", "MYA"};
         String[] DRVAliases = {"Vietnam", "Annam", "Cochin China", "Tonkin", "DRV", "VNM", "RVN",};
         String[] TANAliases = {"Tanzania", "Tanganyika", "Zanzibar", "TAZ", "ZAN"};
         String[] USAAliases = {"United States of America", "United States", "USA"};
         String[] DENAliases = {"Denmark", "Greenland", "DEN"};
         String[] CocosAliases = {"Cocos Islands", "Keeling Islands"};
+        String[] BHMAliases = {"Bahamas", "Bahamas, The", "BHM"};
+        String [] DAMAliases = {"Dominica", "DMA"};
+        String [] GRNAliases = {"Grenada", "GRN"};
+        String [] SLUAliases = {"Saint Lucia", "SLU"};
 
         ArrayList<String[]> exceptionsList = new ArrayList<String[]>();
         exceptionsList.add(DRCAliases);
@@ -114,8 +167,12 @@ public class IRoadTrip {
         exceptionsList.add(USAAliases);
         exceptionsList.add(DENAliases);
         exceptionsList.add(CocosAliases);
+        exceptionsList.add(BHMAliases);
 
         for (String[] aliasList : exceptionsList) {
+            Country c = new Country(aliasList[0]);
+            countriesGraph.put(aliasList[0], c);
+
             for (String val : aliasList) {
                 nameDict.put(val, aliasList[0]);
             }
@@ -123,7 +180,7 @@ public class IRoadTrip {
     }
 
     //Handle () aliases as well
-    private void generateAliasEntries(String toTokenise) {
+    private String generateAliasEntries(String toTokenise) {
         ArrayList<String> toAdd = new ArrayList<String>();
 
         String[] alias = toTokenise.split("\\(|/", 0);
@@ -137,13 +194,28 @@ public class IRoadTrip {
         
         String repVal = toAdd.get(0);
 
+        if (!countriesGraph.containsKey(repVal)) {
+            Country c = new Country(repVal);
+            countriesGraph.put(repVal, c);
+        }
+
         //if state is not in dictionary, add it with representative country name as value
         for (String stateName : toAdd) {
             if (!nameDict.containsKey(stateName)) {
                 nameDict.put(stateName, repVal);
-                //System.out.printf("nameDict[%s]: %s\n", stateName, nameDict.get(stateName));
-                }
             }
+        }
+        return repVal;
+    }
+
+    public void viewHashMap() {
+        Set keys = countriesGraph.keySet();
+        Iterator i = keys.iterator();
+
+        while (i.hasNext()) {
+            Object key = i.next();
+            System.out.println("countriesGraph[" + key + "]: " + countriesGraph.get(key));
+        }
     }
 
     /*
